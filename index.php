c80fdda0672a68a9227551c37b0ba4d757dd243a
rafatq <RApollo2@gmail.com>
2017-05-19 16:18:26 +0200

    index php 3.0

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	require_once ('php/db_settings.php');
 	require_once ('php/db_connect.php');
 	require_once ('php/theme_staff.php');
+	require_once ('php/functions.php');
 
 	
 	$template = new ThemeStaff('show');
@@ -12,39 +13,66 @@
 <!doctype html>
 <html lang="pl">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="css.css" type="text/css" />
+<meta charset="UTF-8">
+<meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">
+
 <title>MotoLokalizator - <?php echo $template->AddSiteName(); ?></title>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="js/global.js"></script>
+<link rel="stylesheet" href="css.css" type="text/css" />
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.css" />
+<?php 
+if(checkUA()){
+echo '<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>'; 
+echo '<script src="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.js"></script>';
+if ($_GET['id']){
+		echo '<script type="text/javascript" src="js/globalmobile.js"></script>';
+	}
+}
+else {
+	echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>';
+	if ($_GET['id']){
+		echo '<script type="text/javascript" src="js/global.js"></script>';
+	}
+ }
+
+?>
+
+
+
 </head>
 
 <body>
+<div id="main"  data-role="page" data-title="Moto Lokaziator">
 	<div id="content">
-    	<header>
-        	<h1 class="title-h1"><a href="http://polmak.ayz.pl/">MotoLokalizator</a></h1>
+    	<header data-role="header">
+        	<h1 class="title-h1"><a data-role="button" data-icon="home" href="http://polmak.ayz.pl/">MotoLokalizator</a></h1>
             <h2 class="tagline"><?php echo $template->AddSiteName(); ?></h2>
-        	<nav>
+         </header>
+         
+         <section>
+        	<nav data-role="navbar">
             	<div id="navigation">
-            	<ul class="menu">
-                	<li><a href="http://polmak.ayz.pl/">START</a></li>
-            		<li><a href="http://polmak.ayz.pl/?show=offers">OFERTY</a></li>
+            	<ul class="menu" data-role="controlgroup" data-type="horizontal">
+                	<li data-role="button"><a  href="http://polmak.ayz.pl/">START</a></li>
+            		<li data-role="button"><a  href="http://polmak.ayz.pl/?show=offers">OFERTY</a></li>
                 </ul>
                 </div>
             </nav>
-        </header>
+        </section>
         
         <section>
+        	<div data-role="content">
 			<?php $template->IncludeSite(); ?>
+            </div>
 	   </section>
        
        <div id="footer">
-       		<footer>
+       		<footer data-role="footer" data-position="fixed">
             	<h3>MOTOLOKALIZATOR</h3>
                 <h4><?php echo $template->AddSiteName(); ?></h4>
                 <p> Cracow University of Economics &bull; PROJECT &bull; MATEUSZ &amp; RAFAŁ &copy; <?php echo date('Y'); ?></p>
        		</footer>
        </div>
 	</div>
+</div>
 </body>
 </html>
